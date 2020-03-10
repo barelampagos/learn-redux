8093f3338f9311662febcc039cb346979d54df3d
barelampagos <barelampagos@gmail.com>
2020-03-09 22:04:51 -0700

    Example for removing from array state

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -38,6 +38,11 @@ var reducer = (state = stateDefault, action) => {
 					}
 				]
 			};
+		case 'REMOVE_HOBBY':
+			return {
+				...state,
+				hobbies: state.hobbies.filter(hobby => hobby.id !== action.id)
+			};
 		case 'ADD_MOVIE':
 			return {
 				...state,
@@ -49,6 +54,11 @@ var reducer = (state = stateDefault, action) => {
 						genre: action.genre
 					}
 				]
+			};
+		case 'REMOVE_MOVIE':
+			return {
+				...state,
+				movies: state.movies.filter(movie => movie.id !== action.id)
 			};
 		default:
 			return state;
@@ -86,6 +96,16 @@ store.dispatch({
 });
 
 store.dispatch({
+	type: 'ADD_HOBBY',
+	hobby: 'Walking'
+});
+
+store.dispatch({
+	type: 'REMOVE_HOBBY',
+	id: 2
+});
+
+store.dispatch({
 	type: 'CHANGE_NAME',
 	name: 'Emily'
 });
@@ -100,4 +120,9 @@ store.dispatch({
 	type: 'ADD_MOVIE',
 	title: 'Avengers',
 	genre: 'Adventure'
+});
+
+store.dispatch({
+	type: 'REMOVE_MOVIE',
+	id: 1
 });
